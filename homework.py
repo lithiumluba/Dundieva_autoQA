c4f04006bc45b655208323c4940a9af583a9c84b
lithiumluba <lithiumluba@gmail.com>
2024-03-26 18:02:14 +0200

    homework added

---
+++ homework.py
@@ -0,0 +1,24 @@
+message = 1
+mesSage = 22
+message_one = 3
+message2 = 99
+_message = 1
+messageForYou = 8
+# перевірити чи перша змінна більша другої змінної
+print("a")
+print(message > mesSage)
+# перевірити чи перша змінна менша другої змінної
+print("b")
+print(message < mesSage)
+# перевірку чи число не нуль
+print("c")
+print(_message != 0)
+# перевірити чи дві змінні дорівнюють одна одній
+print("d")
+print(messageForYou == message2)
+# перевірити чи дві змінні не дорівнюють одна одній
+print("e")
+print(message_one != message2)
+# креатив
+print("f")
+print((message_one + messageForYou * _message) != message2)
